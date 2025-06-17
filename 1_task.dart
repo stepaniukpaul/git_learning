38a1da76629f46b1bbba3390a85e420c44825765
stepaniukpaul <poul.apple@gmail.com>
2025-06-17 12:48:01 +0300

    1 task: math operators

--- 1_task.dart
+++ 1_task.dart
@@ -1,1 +1,15 @@
-void main() {}
+void main() {
+  mathOperators();
+}
+
+void mathOperators() {
+  double width = 18.4;
+  double height = 10.9;
+  int oneSquareWidth = 1;
+  int oneSquareHeight = 1;
+  int oneSquare = oneSquareWidth * oneSquareHeight;
+
+  print('Периметр = ${(width + height) * 2}');
+  print('Площа = ${width * height}');
+  print('Повних квадратів у площині = ${width * height ~/ oneSquare}');
+}
